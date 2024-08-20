8c15f67e52b3f5d28c949bb69889a4b1a7ef5786
Nati Kamusher <natan.kamusher@gmail.com>
2024-08-20 04:08:36 +0300

    cleanup

--- sum.test.js
+++ sum.test.js
@@ -5,22 +5,4 @@ describe("sum part 1", () => {
   it("adds two numbers", () => {
     expect(sum(1, 2)).toBe(3);
   });
-
-  it("adds zero", () => {
-    expect(sum(1, 0)).toBe(1);
-  });
-});
-
-describe("sum part 2", () => {
-  it("adds negative numbers", () => {
-    expect(sum(1, -1)).toBe(0);
-  });
-
-  it("adds large numbers", () => {
-    expect(sum(1000, 2000)).toBe(3000);
-  });
-
-  it("adds floating point numbers", () => {
-    expect(sum(1.5, 2.5)).toBe(4);
-  });
 });
