e8284034c58ac8a0e833fc61511d24021de95cce
Nati Kamusher <natan.kamusher@gmail.com>
2024-08-20 04:09:13 +0300

    update test suite description

--- sum.test.js
+++ sum.test.js
@@ -1,7 +1,7 @@
 import { describe, it, expect } from "vitest";
 import { sum } from "./sum";
 
-describe("sum part 1", () => {
+describe("sum", () => {
   it("adds two numbers", () => {
     expect(sum(1, 2)).toBe(3);
   });
