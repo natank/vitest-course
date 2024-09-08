9ac3ada18b4139b242b11bd8fa7e817b985a593d
Nati Kamusher <natan.kamusher@gmail.com>
2024-09-08 14:10:04 +0300

    added more tests

--- sum.test.js
+++ sum.test.js
@@ -5,10 +5,16 @@ describe("sum part 1", () => {
   it("adds two numbers", () => {
     expect(sum(1, 2)).toBe(3);
   });
-  it("adds zero", () => {
+  it("adds zero to number", () => {
     expect(sum(0, 2)).toBe(2);
   });
   it("adding negative numbers", () => {
     expect(sum(-1, -1)).toBe(-2);
+  });
+  it("adding floating numbers", () => {
+    expect(sum(0.1, 0.2)).toBeCloseTo(0.3);
+  });
+  it("adding large numbers", () => {
+    expect(sum(10000, 20000)).toBe(30000);
   });
 });
